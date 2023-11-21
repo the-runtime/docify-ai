24589d8cf28b9d8a7a78ed0aa8c6526161c4a5f9
theruntime <theruntime7@gmail.com>
2023-11-21 12:14:12 +0530

    minor chnages

--- worker_main.py
+++ worker_main.py
@@ -1,10 +1,11 @@
 from docifyai.core import logger
 from server.workers import worker
+
 logger = logger.Logger(__name__)
 
 if __name__ == "__main__":
     logger.info("RQ worker started.")
     try:
-        worker.run_workers("queue_for_jobs_to_worker")
+        worker.run_workers("queue_for_jobs_to_worker")  # get from env file rather than hard code it
     except KeyboardInterrupt:
         logger.info("RQ worker stopped buy user.")
